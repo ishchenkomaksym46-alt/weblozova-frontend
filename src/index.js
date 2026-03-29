ce1a42cf24a3a4b060000a7a12aff8f97370f9f2
Max <ishchenkomaksym46@gmail.com>
2026-03-29 13:10:29 +0300

    Start

---
+++ src/index.js
@@ -0,0 +1,12 @@
+import { Route, Routes, BrowserRouter } from 'react-router-dom';
+import ReactDOM from 'react-dom/client';
+
+const root = ReactDOM.createRoot(document.getElementById('root'));
+
+root.render(
+  <BrowserRouter>
+    <Routes>
+      <Route />
+    </Routes>
+  </BrowserRouter>
+);
